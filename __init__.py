41cba3307ec7886bbfbe40b591c84a5a0e1493e7
Null <pairmanxlr@gmail.com>
2024-04-01 02:19:10 +0800

    remove type hint

--- __init__.py
+++ __init__.py
@@ -1,37 +1,37 @@
-def int_overflow(n: int):
+def int_overflow(n):
 	"""Overlow behavior of signed int32 type.
- 	:param n: Number to be owerflowed.
-  	:return: The overflowed number.
+ 	:param n: Number to be owerflowed. Int.
+  	:return: The overflowed number. Int.
  	"""
 	return (n + 0x80000000) & 0xFFFFFFFF - 0x80000000
 
-def lshift(n: int | float, i: int):
+def lshift(n, i):
 	"""JavaScript-flavored bitwise shift left (<<).
- 	:param n: Number to be shifted.
- 	:param i: Number of bits to shift by.
-  	:return: The shifted number.
+ 	:param n: Number to be owerflowed. Int or float.
+ 	:param i: Number of bits to shift by. Int.
+  	:return: The shifted number. Int.
  	"""
 	n = (type(n) is float and int(n) & 0xFFFFFFFF) or n
 	i &= 0x1F
 	j = abs(i)
 	return int_overflow((i >= 0 and n << j) or n >> j)
 
-def rshift(n: int | float, i: int):
+def rshift(n, i):
 	"""JavaScript-flavored bitwise shift right (>>).
- 	:param n: Number to be shifted.
- 	:param i: Number of bits to shift by.
-  	:return: The shifted number.
+ 	:param n: Number to be owerflowed. Int or float.
+ 	:param i: Number of bits to shift by. Int.
+  	:return: The shifted number. Int.
  	"""
 	n = (type(n) is float and int(n) & 0xFFFFFFFF) or n
 	i &= 0x1F
 	j = abs(i)
 	return int_overflow((i >= 0 and n >> j) or n << -j)
 
-def urshift(n: int | float, i: int):
+def urshift(n, i):
 	"""JavaScript-flavored bitwise unsigned shift right (>>>).
- 	:param n: Number to be shifted.
- 	:param i: Number of bits to shift by.
-  	:return: The shifted number.
+ 	:param n: Number to be owerflowed. Int or float.
+ 	:param i: Number of bits to shift by. Int.
+  	:return: The shifted number. Int.
  	"""
 	n = (type(n) is float and int(n) & 0xFFFFFFFF) or n
 	n &= (n < 0 and 0xFFFFFFFF) or n
