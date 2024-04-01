3e7f0f7423b033e8dfa40f131fbf886f78022c5e
Null <pairmanxlr@gmail.com>
2024-04-01 08:59:24 +0800

    overload. todo: type hint

--- src/pyjsshift/__init__.py
+++ src/pyjsshift/__init__.py
@@ -1,3 +1,5 @@
+from typing import overload
+
 def int_overflow(n):
 	"""Overlow behavior of signed int32 type.
  	:param n: Number to be owerflowed. Int.
@@ -5,6 +7,18 @@ def int_overflow(n):
  	"""
 	return (n + 0x80000000) & 0xFFFFFFFF - 0x80000000
 
+@overload
+def lshift(n, i):
+	"""JavaScript-flavored bitwise shift left (<<).
+ 	:param n: Number to be owerflowed. Int or float.
+ 	:param i: Number of bits to shift by. Int.
+  	:return: The shifted number. Int.
+ 	"""
+	i &= 0x1F
+	j = abs(i)
+	return int_overflow((i >= 0 and n << j) or n >> j)
+
+@overload
 def lshift(n, i):
 	"""JavaScript-flavored bitwise shift left (<<).
  	:param n: Number to be owerflowed. Int or float.
@@ -16,6 +30,7 @@ def lshift(n, i):
 	j = abs(i)
 	return int_overflow((i >= 0 and n << j) or n >> j)
 
+@overload
 def rshift(n, i):
 	"""JavaScript-flavored bitwise shift right (>>).
  	:param n: Number to be owerflowed. Int or float.
@@ -27,6 +42,30 @@ def rshift(n, i):
 	j = abs(i)
 	return int_overflow((i >= 0 and n >> j) or n << -j)
 
+@overload
+def rshift(n, i):
+	"""JavaScript-flavored bitwise shift right (>>).
+ 	:param n: Number to be owerflowed. Int or float.
+ 	:param i: Number of bits to shift by. Int.
+  	:return: The shifted number. Int.
+ 	"""
+	i &= 0x1F
+	j = abs(i)
+	return int_overflow((i >= 0 and n >> j) or n << -j)
+
+@overload
+def urshift(n, i):
+	"""JavaScript-flavored bitwise unsigned shift right (>>>).
+ 	:param n: Number to be owerflowed. Int or float.
+ 	:param i: Number of bits to shift by. Int.
+  	:return: The shifted number. Int.
+ 	"""
+	n &= (n < 0 and 0xFFFFFFFF) or n
+	i &= 0x1F
+	j = abs(i)
+	return int_overflow(n >> j) if i >= 0 else -int_overflow(n << j)
+
+@overload
 def urshift(n, i):
 	"""JavaScript-flavored bitwise unsigned shift right (>>>).
  	:param n: Number to be owerflowed. Int or float.
